5f302c2a5bd108065509e4f6691f12162ccf6d6c
luzatamarihuaricacha-hash <luzatamarihuaricacha@gmail.com>
2026-02-25 12:28:53 -0500

    first commit

---
+++ 1.-tipos -de-datos.py
@@ -0,0 +1,23 @@
+#tipos de datos en python
+
+# 1. tipo de dato numerico (int)
+edad = 25
+promedio = 19
+numero = -50
+
+#2. tipo de dato decimal (float)
+peso = 75.50
+estatura = 170.30
+temperatura = -14.70
+precio = 99.99
+
+#3. tipo de dato texto (str)
+letra = 'A'
+nombre = 'Luz'
+apellido = "Luque"
+dni = '70295792'
+
+# 4. tipo de dato booleanos (bool)
+es_mayo_de_edad = True
+puede_volar = False
+
---
+++ 2.operadores.py
@@ -0,0 +1,45 @@
+#Operadores en python
+
+#1. Operadores Aritmeticos
+suma = 5 + 5
+resta = 10 - 6
+multiplicacion = 5 * 5
+division = 10 / 2
+modulo = 10 % 2
+exponente = 10 ** 2
+
+
+print ('El resultado de la suma es: ',suma)
+print ('El resultado de la resta es: ',resta)
+print ('El resultado de la multiplicacion es: ',multiplicacion)
+print ('El resultado de la division es: ',division)
+print ('El resultado de la modulo es: ',modulo)
+print ('El resultado de la exponente es: ',exponente)
+
+#2. Operadores de comparacion
+print( 5 == 5 )    #Igual a
+print( 5 != 5 )    #Diferente de 
+print( 10 > 5 )    #Mayor que
+print( 10 < 5 )    #Menor que
+print( 10 >= 5 )   #Mayor o igual que
+print( 10 <=5 )    #Menor o igual que
+
+#3.Operadores logicos
+v = True
+f = False
+ #3.1. and (y)
+print('-----------------------------------AND')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+ #3.2. or (o)
+print('-----------------------------------OR')
+print(v or v)
+print(v or f)
+print(f or v)
+print(f or f)
+ #3.3. not (negacion)
+print('-----------------------------------NOT')
+print(not v)
+print(not f)
